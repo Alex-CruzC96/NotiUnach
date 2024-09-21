40ac6936e991c5ec26e030ea0eb80b57d1bdff1a
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-09-21 12:08:47 -0600

    Se agregó el enrutamiento de algunas cosas

--- notiUnach/src/App.jsx
+++ notiUnach/src/App.jsx
@@ -4,6 +4,7 @@ import Post from './components/Post'
 import AsideDesktop from './components/AsideDesktop'
 import './App.css'
 import { useState } from 'react'
+import {BrowserRouter,Route, Routes} from 'react-router-dom'
 
 function App() {
 
@@ -23,29 +24,38 @@ function App() {
 
   return (
     <>
-      <div id="header" className={dark}>
-        { isDesktop ? (<Header/>) : (<AsideDesktop darkMode={darkMode} setDarkMode={setDarkMode}/>)}
-      </div>
-      <div id='post' className={dark}>
-        <Post user={'github/mdo'} name={'User_00'} date={'14-09-2024'} content={'Vendo libro usado, como nuevo. $200 pesos a tratar.'}/>
-        <Post user={'duckduckgo/gummibeer.dev'} name={'User_01'} date={'13-09-2024'} content={'Busco libro de inglés para un nivel 2.'}/>
-        <Post user={'google/netflix.com'} name={'User_02'} date={'13-09-2024'} content={'Soy estudiante de arquitectura y estoy vendiendo un set de reglas'}/>
-        <Post 
-        user={'readcv/elenatorro'} 
-        name={'User_03'} 
-        date={'12-09-2024'}
-        content={'Mi primer post en la página, ¡Qué emoción!'}
-        />
-        <Post 
-        user={'x/midudev'} 
-        name={'miDudev'} 
-        date={'09-08-2024'}
-        content={'Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi nec interdum integer taciti. Ridiculus neque posuere nulla sociosqu, rhoncus ac fusce curabitur, conubia eget metus. Metus sagittis arcu molestie enim luctus in praesent auctor, faucibus pellentesque ligula penatibus conubia et tempor risus elementum, dui integer ac habitant blandit pretium tristique. Aliquet quam velit augue sem morbi mauris sagittis iaculis risus pharetra porttitor cras, vel et integer est nulla placerat odio vestibulum montes varius. Natoque faucibus tempus a lobortis conubia ad risus elementum phasellus, mattis scelerisque suscipit porttitor hendrerit arcu parturient interdum, odio montes inceptos ornare neque posuere tempor non. Neque suspendisse curae donec quam facilisi imperdiet eleifend torquent aliquam porta, a sagittis himenaeos vulputate ante bibendum cras ridiculus pulvinar, vitae augue habitant nascetur fringilla diam suscipit enim cum.'}
-        />
-      </div>
-      <div id='footer'>
-        { isDesktop ? (<Footer/>) : '' }
-      </div>
+      <BrowserRouter>
+          <div id="header" className={dark}>
+            { isDesktop ? (<Header/>) : (<AsideDesktop darkMode={darkMode} setDarkMode={setDarkMode}/>)}
+          </div>
+
+        <Routes>
+          <Route path='/' element={
+            <div id='post' className={dark}>
+              <Post user={'github/mdo'} name={'User_00'} date={'14-09-2024'} content={'Vendo libro usado, como nuevo. $200 pesos a tratar.'}/>
+              <Post user={'duckduckgo/gummibeer.dev'} name={'User_01'} date={'13-09-2024'} content={'Busco libro de inglés para un nivel 2.'}/>
+              <Post user={'google/netflix.com'} name={'User_02'} date={'13-09-2024'} content={'Soy estudiante de arquitectura y estoy vendiendo un set de reglas'}/>
+              <Post 
+              user={'readcv/elenatorro'} 
+              name={'User_03'} 
+              date={'12-09-2024'}
+              content={'Mi primer post en la página, ¡Qué emoción!'}
+              />
+              <Post 
+              user={'x/midudev'} 
+              name={'miDudev'} 
+              date={'09-08-2024'}
+              content={'Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi nec interdum integer taciti. Ridiculus neque posuere nulla sociosqu, rhoncus ac fusce curabitur, conubia eget metus. Metus sagittis arcu molestie enim luctus in praesent auctor, faucibus pellentesque ligula penatibus conubia et tempor risus elementum, dui integer ac habitant blandit pretium tristique. Aliquet quam velit augue sem morbi mauris sagittis iaculis risus pharetra porttitor cras, vel et integer est nulla placerat odio vestibulum montes varius. Natoque faucibus tempus a lobortis conubia ad risus elementum phasellus, mattis scelerisque suscipit porttitor hendrerit arcu parturient interdum, odio montes inceptos ornare neque posuere tempor non. Neque suspendisse curae donec quam facilisi imperdiet eleifend torquent aliquam porta, a sagittis himenaeos vulputate ante bibendum cras ridiculus pulvinar, vitae augue habitant nascetur fringilla diam suscipit enim cum.'}
+              />
+            </div>
+            }>
+          </Route>
+        </Routes>
+        
+        <div id='footer'>
+          { isDesktop ? (<Footer/>) : '' }
+        </div>
+      </BrowserRouter>
     </>
   )
 }
--- notiUnach/src/components/AsideDesktop.jsx
+++ notiUnach/src/components/AsideDesktop.jsx
@@ -11,64 +11,73 @@ import { faMoon } from "@fortawesome/free-solid-svg-icons"
 import { faSun } from "@fortawesome/free-solid-svg-icons"
 import './AsideDesktop.css'
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
-const AsideDesktop = ({darkMode,setDarkMode}) => {
-    
+const AsideDesktop = ({ darkMode, setDarkMode }) => {
+
     //Variables que sirven para el renderizado de ciertos componentes y sus animaciones
-    const[hoverInicio,setHoverInicio]=useState(false);
-    const[hoverCat,setHoverCat]=useState(false);
-    const[hoverNoti,setHoverNoti]=useState(false);
-    const[hoverConf,setHoverConf]=useState(false);
-    
+    const [hoverInicio, setHoverInicio] = useState(false);
+    const [hoverCat, setHoverCat] = useState(false);
+    const [hoverNoti, setHoverNoti] = useState(false);
+    const [hoverConf, setHoverConf] = useState(false);
+
     return (
         <>
             <div className="container-fluid">
                 <div className="row text-center">
                     <div className="col">
-                        <Image className="imageProfile" fluid src={ImgProfile+'github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4'} roundedCircle/>
+                        <Image className="imageProfile" fluid src={ImgProfile + 'github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4'} roundedCircle />
                     </div>
                 </div>
                 <div className="row my-4 px-4 options">
                     <div className="col p-3 ps-3 rounded-pill addPost">
-                        <FontAwesomeIcon icon={faCirclePlus} size="lg" className="tag"/>
+                        <FontAwesomeIcon icon={faCirclePlus} size="lg" className="tag" />
                         <p className="m-0 ms-2 d-inline tag">Publicar</p>
                     </div>
                 </div>
                 <div className="row mt-3 row-cols-1 g-3 p-3 pt-0 iconos">
                     <div className="col">
-                        <div className="options" onMouseEnter={()=>setHoverInicio(true)} onMouseLeave={()=>setHoverInicio(false)}>
-                            {hoverInicio ? <FontAwesomeIcon icon={faHouse} bounce className="tag"/> : <FontAwesomeIcon icon={faHouse} className="tag"/>}
-                            <p className="d-inline ms-2 tag">Inicio</p>
-                        </div>
+                        <Link to="/" className="no-decoration">
+                            <div className="options" onMouseEnter={() => setHoverInicio(true)} onMouseLeave={() => setHoverInicio(false)}>
+                                {hoverInicio ? <FontAwesomeIcon icon={faHouse} bounce className="tag" /> : <FontAwesomeIcon icon={faHouse} className="tag" />}
+                                <p className="d-inline ms-2 tag">Inicio</p>
+                            </div>
+                        </Link>
                     </div>
                     <div className="col">
-                        <div className="options" onMouseEnter={()=>setHoverCat(true)} onMouseLeave={()=>setHoverCat(false)}>
-                            {hoverCat ? <FontAwesomeIcon icon={faBars} flip className="tag"/> : <FontAwesomeIcon icon={faBars} className="tag"/>}
-                            <p className="d-inline ms-2 tag">Categorías</p>
-                        </div>
+                        <Link to="/categorias" className="no-decoration">
+                            <div className="options" onMouseEnter={() => setHoverCat(true)} onMouseLeave={() => setHoverCat(false)}>
+                                {hoverCat ? <FontAwesomeIcon icon={faBars} flip className="tag" /> : <FontAwesomeIcon icon={faBars} className="tag" />}
+                                <p className="d-inline ms-2 tag">Categorías</p>
+                            </div>
+                        </Link>
                     </div>
                     <div className="col">
-                        <div className="options" onMouseEnter={()=> setHoverNoti(true)} onMouseLeave={()=>setHoverNoti(false)}>
-                            {hoverNoti ? <FontAwesomeIcon icon={faBell} shake className="tag"/> : <FontAwesomeIcon icon={faBell} className="tag"/>}
-                            <p className="d-inline ms-2 tag">Notificaciones</p>
-                        </div>
+                        <Link to="/notificaciones" className="no-decoration">
+                            <div className="options" onMouseEnter={() => setHoverNoti(true)} onMouseLeave={() => setHoverNoti(false)}>
+                                {hoverNoti ? <FontAwesomeIcon icon={faBell} shake className="tag" /> : <FontAwesomeIcon icon={faBell} className="tag" />}
+                                <p className="d-inline ms-2 tag">Notificaciones</p>
+                            </div>
+                        </Link>
                     </div>
                     <div className="col">
-                        <div className="options" onMouseEnter={()=>setHoverConf(true)} onMouseLeave={()=>setHoverConf(false)}>
-                            {hoverConf ? <FontAwesomeIcon icon={faGear} spin className="tag"/> : <FontAwesomeIcon icon={faGear} className="tag"/>}
-                            <p className="d-inline ms-2 tag">Configuración</p>
-                        </div>
+                        <Link to="/configuracion" className="no-decoration">
+                            <div className="options" onMouseEnter={() => setHoverConf(true)} onMouseLeave={() => setHoverConf(false)}>
+                                {hoverConf ? <FontAwesomeIcon icon={faGear} spin className="tag" /> : <FontAwesomeIcon icon={faGear} className="tag" />}
+                                <p className="d-inline ms-2 tag">Configuración</p>
+                            </div>
+                        </Link>
                     </div>
 
                     <div className="row position-absolute start-0 bottom-0 asideFoot py-3">
                         <div className="col text-center">
-                            <FontAwesomeIcon icon={faSun} size="lg"/>
+                            <FontAwesomeIcon icon={faSun} size="lg" />
                             <Form.Check // prettier-ignore
-                            type="switch"
-                            className="d-inline mx-3"
-                            onChange={()=>setDarkMode(!darkMode)}
+                                type="switch"
+                                className="d-inline mx-3"
+                                onChange={() => setDarkMode(!darkMode)}
                             />
-                            <FontAwesomeIcon icon={faMoon} size="lg"/>
+                            <FontAwesomeIcon icon={faMoon} size="lg" />
                         </div>
                     </div>
                 </div>
