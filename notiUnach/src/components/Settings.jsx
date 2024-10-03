6f6957fadbd6cec9515aeb916423aeeb1b01ee92
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-03 12:43:03 -0600

    Ya aparece configuracion de usuario

--- notiUnach/src/components/Settings.jsx
+++ notiUnach/src/components/Settings.jsx
@@ -1,6 +1,8 @@
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import './Settings.css'
 
 const Settings=()=>{
@@ -11,7 +13,14 @@ const Settings=()=>{
                 <p className="fs-2">Configuración</p>
 
                 <Row className="mt-5 mx-2 p-3 text-start setting-option">
-                    <Col>Hola mundo</Col>
+                    <Col>
+                        <div className="border border-2 rounded-circle d-inline-flex align-items-center justify-content-center circulo-opcion">
+                            <FontAwesomeIcon icon={faUser} size="lg"/>
+                        </div>
+                        <span className="ms-3 d-inline-block">
+                            <p className="fs-5 m-0">Configuración de usuario</p>
+                        </span>
+                    </Col>
                 </Row>
             </Container>
         </>
