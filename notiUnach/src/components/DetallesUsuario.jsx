07241a8555b1b2f1ca1b608b85b54c15a1638b5b
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-04 17:04:38 -0600

    Estoy trabajando con detalles de usuario

--- notiUnach/src/components/DetallesUsuario.jsx
+++ notiUnach/src/components/DetallesUsuario.jsx
@@ -1,11 +1,27 @@
 import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Form from 'react-bootstrap/Form';
 
 const DetalleUsuario=()=>{
 
     return (
         <>
-            <Container fluid>
-                <p className="fs-4">Hola mundo!!!</p>
+            <Container fluid className="pt-4">
+                <Row className="row-cols-1">
+                    <Col>
+                        <p className="fs-4">Actualizar foto de perfil</p>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Control type="file" className="bg-transparent archive"/>
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        asdasd
+                    </Col>
+                </Row>
+                
             </Container>
         
         </>
