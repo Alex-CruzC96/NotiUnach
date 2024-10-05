ac16a0c1af61177e43f471f32ffa3fc4528e6943
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-04 18:17:55 -0600

    El modal esta en modo obscuro

--- notiUnach/src/components/DetallesUsuario.jsx
+++ notiUnach/src/components/DetallesUsuario.jsx
@@ -2,8 +2,16 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
 
-const DetalleUsuario=()=>{
+const DetalleUsuario = () => {
+
+    //Necesarias para modal
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <>
@@ -14,16 +22,31 @@ const DetalleUsuario=()=>{
                     </Col>
                     <Col>
                         <Form.Group controlId="formFile" className="mb-3">
-                            <Form.Control type="file" className="bg-transparent archive"/>
+                            <Form.Control type="file" className="bg-transparent archive" />
                         </Form.Group>
                     </Col>
-                    <Col>
-                        asdasd
+                    <Col className="">
+                        <Button variant="secondary" onClick={handleShow}>Elegir imagen previamente subida</Button>{' '}
                     </Col>
                 </Row>
-                
+
             </Container>
-        
+
+            <Modal show={show} onHide={handleClose} data-bs-theme="dark">
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
         </>
     );
 }
