f15f9633b243bc3e06b89613698205865e29332d
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-12 17:41:09 -0600

    Se comienza a trabajar con el backend de la aplicación

--- notiUnach/backend/routes/signup.js
+++ notiUnach/backend/routes/signup.js
@@ -1,6 +1,20 @@
+const { jsonResponse } = require('../lib/jsonResponse');
+
 const router=require('express').Router();
 
-router.get("/",(req,res)=>{
+router.post("/",(req,res)=>{
+    const {name,mail,password} =req.body;
+
+    if(!!name || !!mail || !!password){
+        return res.status(400).json(jsonResponse(400,{
+            error : "Los campos son obligatorios"
+        }));
+    }
+
+    //Creación de usuario
+    res.status(200).json(jsonResponse(200,{
+        message:"Usuario creado con éxito!!"
+    }))
     res.send("SignUp");
 });
 
