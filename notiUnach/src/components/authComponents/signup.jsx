a277c9c682a83654d8b651c1262663cacf84561a
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-12 17:00:38 -0600

    Se les agregó un useState a todos los inputs del signup

--- notiUnach/src/components/authComponents/signup.jsx
+++ notiUnach/src/components/authComponents/signup.jsx
@@ -7,8 +7,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import './signup.css'
+import { useState } from "react";
 
 export default function SignUp() {
+    
+    const [name,setName]=useState('');
+    const [lastName,setLastName]=useState('');
+    const [mail,setMail]=useState('');
+    const [password,setPassword]=useState('');
+    const [confirmPass,setConfirmPass]=useState('');
+
     return (
         <>
             <Container fluid id="cajaContenedora" className="d-flex justify-content-center align-items-center">
@@ -24,23 +32,23 @@ export default function SignUp() {
                             <Form>
                                 <Form.Group className="mb-1" controlId="formGroupName">
                                     <Form.Label>Nombre de usuario</Form.Label>
-                                    <Form.Control type="text" placeholder="Ingrese su nombre"/>
+                                    <Form.Control type="text" placeholder="Ingrese su nombre" value={name} onChange={(e)=>{setName(e.target.value)}}/>
                                 </Form.Group>
                                 <Form.Group className="mb-1" controlId="formGroupLastname">
                                     <Form.Label>Apellido de usuario</Form.Label>
-                                    <Form.Control type="text" placeholder="Ingrese sus apellidos"/>
+                                    <Form.Control type="text" placeholder="Ingrese sus apellidos" value={lastName} onChange={(e)=>{setLastName(e.target.value)}}/>
                                 </Form.Group>
                                 <Form.Group className="mb-1" controlId="formGroupEmail">
                                     <Form.Label>Correo electrónico</Form.Label>
-                                    <Form.Control type="email" placeholder="Ingrese su correo electrónico" />
+                                    <Form.Control type="email" placeholder="Ingrese su correo electrónico" value={mail} onChange={(e)=>{setMail(e.target.value)}}/>
                                 </Form.Group>
                                 <Form.Group className="mb-1" controlId="formGroupPassword">
                                     <Form.Label>Contraseña</Form.Label>
-                                    <Form.Control type="password" placeholder="Ingrese su contraseña" />
+                                    <Form.Control type="password" placeholder="Ingrese su contraseña" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
                                 </Form.Group>
                                 <Form.Group className="mb-1" controlId="formGroupConfirmPassword">
                                     <Form.Label>Confirmar contraseña</Form.Label>
-                                    <Form.Control type="password" placeholder="Ingrese su contraseña" />
+                                    <Form.Control type="password" placeholder="Ingrese su contraseña" value={confirmPass} onChange={(e)=>{setConfirmPass(e.target.value)}}/>
                                 </Form.Group>
                                 <Form.Group className="text-center mt-2">
                                     <Row>
