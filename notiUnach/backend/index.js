304396b44b44497380a12386d5a48dc839ab2b40
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-12 17:33:32 -0600

    Comencé con los primeros pasos de la API

---
+++ notiUnach/backend/index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const sql=require('mysql');
+
+require('dotenv').config();
+
+const port=process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/signup',require('./routes/signup'));
+app.use('/api/login',require('./routes/login'));
+app.use('/api/signout',require('./routes/signout'));
+
+app.get('/',(req,res)=>{
+    res.send({status:200});
+});
+
+app.listen(port,()=>{
+    console.log('Server is running without problems!! port: '+port);
+});
