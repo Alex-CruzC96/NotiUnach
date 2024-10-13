9bc0be1d4de6484c486b576805e0836549753d1c
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-12 19:36:40 -0600

    Se estableció la conexión a la base de datos
    Se creó un archivo .env y se instaló la paquetería "dotenv", para poder tener una conexión exitosa con la base de datos, favor de crear el archivo ".env" y llenar los requisitos solicitados

--- notiUnach/backend/index.js
+++ notiUnach/backend/index.js
@@ -5,6 +5,14 @@ const sql=require('mysql');
 
 require('dotenv').config();
 
+const db=sql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT    
+});
+
 const port=process.env.PORT || 5000;
 
 app.use(cors());
@@ -18,6 +26,16 @@ app.get('/',(req,res)=>{
     res.send({status:200});
 });
 
-app.listen(port,()=>{
-    console.log('Server is running without problems!! port: '+port);
+db.connect((err) => {
+    if (err) {
+        console.error('Error en la conexión con la base de datos:', err);
+        process.exit(1); // Salir del proceso si hay un error en la conexión
+    } else {
+        console.log('Conexión exitosa a la DB!!!');
+
+        // Iniciar el servidor solo si la conexión es exitosa
+        app.listen(port, () => {
+            console.log('Server is running without problems!! port: ' + port);
+        });
+    }
 });
