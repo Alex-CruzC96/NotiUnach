ecda95a0e6a7f19c60ca9fff093e279f145221b4
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-18 16:39:35 -0600

    Se terminó el apartado de Login

--- notiUnach/backend/routes/login.js
+++ notiUnach/backend/routes/login.js
@@ -1,6 +1,9 @@
 const { jsonResponse } = require('../lib/jsonResponse');
-
 const router=require('express').Router();
+const db=require('../lib/db');
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+require('dotenv').config();
 
 router.post("/",(req,res)=>{
     const {mail,password} =req.body;
@@ -11,21 +14,68 @@ router.post("/",(req,res)=>{
         }));
     }
 
-    //Autenticarlo
-    const accesToken="acces_token";
-    const refreshToken="refres_token";
-    const user={
-        id:'1',
-        name:'Alex',
-        lastName:'Cruz'
-    }
+    //Verificación de que el usuario exisa en la base de datos
+    const query=`SELECT * FROM User WHERE mail = ?`;
+    db.query(query,[mail],async (err,results)=>{
+        if(err){
+            console.error('Error en la verificación: '+err.message);
+            return res.status(500).json(jsonResponse(500,{
+                error:"Error en el proceso de verificación"
+            }));
+        }
 
-    res.status(200).json(jsonResponse(200,{
-        user,
-        accesToken,
-        refreshToken
-    }));
-    
+        /**
+         * Si el resultado de la consulta da 0
+         * significa que no se encontró el correo
+         * indicado, y por lo tanto, el usuario
+         * no existe
+         */
+        if(results.length===0){
+            return res.status(400).json(jsonResponse(400,{
+                error:"El correo ingresado no está asociado a ningún usuario existente"
+            }));
+        }
+
+        /*
+        * En caso de que el correo ingresado exista realmente
+        entonces significa que el usuario ya está registrado,
+        por lo que, esta variable contendrá los atributos del
+        usuario registrado
+        */
+        const user=results[0];
+
+        //Comparación de la contraseña 
+        const validPassword=await bcrypt.compare(password,user.password);
+
+        //Si la comparación sale mal, entonces la contraseña es incorrecta
+        if(!validPassword){
+            return res.status(400).json(jsonResponse(400,{
+                error:"La contraseña introducida no es correcta"
+            }));
+        }
+
+        //Generación de tokens web
+        const accesToken=jwt.sign({
+            id:user.id,
+            name:user.name,
+            lastName:user.lastName
+        },process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'});
+
+        const refreshToken=jwt.sign({
+            id:user.id
+        },process.env.REFRESH_TOKEN_SECRET,{expiresIn:'7d'});
+
+        //Login exitoso
+        res.status(200).json(jsonResponse(200,{
+            user:{
+                id:user.id,
+                name:user.name,
+                lastName:user.lastName
+            },
+            accesToken,
+            refreshToken
+        }));
+    });
 });
 
 module.exports=router;  
