ebc4d12ba5686c567ce0306410ba12dd00475a11
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-18 15:07:03 -0600

    Ya verifica si el usuario existe antes de hacer una inserción

--- notiUnach/backend/routes/signup.js
+++ notiUnach/backend/routes/signup.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 router.post("/", async (req, res) => {
     const { name, lastName, password, mail } = req.body;
 
-    if (!name || !mail || !password ||!lastName) {
+    if (!name || !mail || !password || !lastName) {
         return res.status(400).json(jsonResponse(400, {
             error: "Los campos son obligatorios"
         }));
@@ -14,27 +14,50 @@ router.post("/", async (req, res) => {
 
     //Creación de usuario
     try {
-        //Hasheo de la password
-        const hashedPassword=await bcrypt.hash(password,10);
-
-        //Query de inserción
-        const query = `INSERT INTO User(name,lastName,password,mail) VALUES(?,?,?,?)`;
-
-        //Ejecución del query
-        db.query(query, [name, lastName, hashedPassword, mail], (err, results) => {
-            //Caso en el que ocurra algún error
-            if(err){
-                console.error('Ha surgido un error al intentar insertar el registro: '+err.message);
-                return res.status(500).json(jsonResponse(500,{
-                    error:'Error al intentar crear el usuario'
+        //Antes de la inserción necesitamos verificar que el usuario no esté ya registrado
+        /**
+         * Para saber si el usuario ya existe realizamos
+         * una consulta en base al correo, ya que el correo
+         * es único para cada usuario
+         */
+        const checkQuery = `SELECT * FROM User WHERE mail = ?`;
+        db.query(checkQuery, [mail], async (err, results) => {
+            if (err) {
+                console.error('Error al verificar el correo electrónico: ' + err.message);
+                return res.status(500).json(jsonResponse(500, {
+                    error: 'Error en el proceso de registro'
                 }));
             }
 
-            //Caso positivo que sí se hizo la inserción
-            res.status(200).json(jsonResponse(200,{
-                message:"Usuario registrado con éxito!!!"
-            }));
+            if (results.length > 0) {
+                return res.status(400).json(jsonResponse(400, {
+                    error: 'El correo ya está registrado'
+                }));
+            }
+
+            //Hasheo de la password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            //Query de inserción
+            const query = `INSERT INTO User(name,lastName,password,mail) VALUES(?,?,?,?)`;
+
+            //Ejecución del query
+            db.query(query, [name, lastName, hashedPassword, mail], (err, results) => {
+                //Caso en el que ocurra algún error
+                if (err) {
+                    console.error('Ha surgido un error al intentar insertar el registro: ' + err.message);
+                    return res.status(500).json(jsonResponse(500, {
+                        error: 'Error al intentar crear el usuario'
+                    }));
+                }
+
+                //Caso positivo que sí se hizo la inserción
+                res.status(200).json(jsonResponse(200, {
+                    message: "Usuario registrado con éxito!!!"
+                }));
+            });
         });
+
     } catch (err) {
         console.error('Ha ocurrido un error inesperado');
         res.status(500).json(jsonResponse(500, {
