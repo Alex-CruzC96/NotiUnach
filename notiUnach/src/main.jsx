26e7ebc0ca9c6be7237b03f2d2b8f7e79dc2fa09
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-19 12:29:12 -0600

    Regrese a AuthProvider al Contexto anterior

--- notiUnach/src/main.jsx
+++ notiUnach/src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { AuthProvider } from './assets/auth/AuthProvider'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
       <App />
-    </AuthProvider>
   </StrictMode>,
 )
