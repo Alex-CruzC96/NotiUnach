aef525dd5bd812c3b11d520a765506d567b049de
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-25 17:58:28 -0600

    Ya podemos actualizar el contenido de data mediante el editor

--- notiUnach/src/components/MakePublication.jsx
+++ notiUnach/src/components/MakePublication.jsx
@@ -2,14 +2,28 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import CK from "./CkEditor/CK"
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
 
 const MakePublication=({dark})=>{
+    const [data,setData]=useState('Hola mundo!!!');
+
+    function showData(){
+        // console.log(CKEditor.getData());
+        alert(data);
+    }
+
     return(
         <>
             <Container fluid className="pt-4">
                 <Row>
                     <Col>
-                        <CK/>
+                        <CK data={data} setData={setData}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button onClick={showData}>Pruebame!!!</Button>
                     </Col>
                 </Row>
             </Container>
