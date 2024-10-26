02142d6e26ccc3b1dcee47e82cb514182c8b1e8e
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-25 20:18:11 -0600

    Se llamó a la API desde el componente

--- notiUnach/src/components/MakePublication.jsx
+++ notiUnach/src/components/MakePublication.jsx
@@ -5,16 +5,44 @@ import CK from "./CkEditor/CK"
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState } from "react";
+import { API_URL } from "../assets/auth/constants";
+import { useAuth } from "../assets/auth/AuthProvider";
 
 const MakePublication=({dark})=>{
     const [data,setData]=useState('');
+    const auth=useAuth();
 
-    function createPost(e){
+    async function createPost(e){
         // console.log(CKEditor.getData());
         e.preventDefault();
-        alert(data);
-
         
+        try{
+            const response=await fetch(`${API_URL}/createPost`,{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json',
+                    'Authorization':`Bearer ${auth.getAccessToken()}`
+                },
+                body:JSON.stringify({
+                    userId:auth.user.id,
+                    body:data
+                })
+            });
+
+            const result=await response.json();
+
+            if(!response.ok){
+                alert('Error al crear el post');
+                return;
+            }
+
+            alert('Publicación creada con éxito!!!');
+        }
+        catch(error){
+            console.error('Ha ocurrido un error al contactar con la API: ',error);
+            alert("Error al crear el post");
+        }
+
     }
 
     return(
