c08ca5deac4788ed0a2f74134d0bc0e2b7c6a296
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-10-29 09:47:56 -0600

    Terminé de programar la consulta donde se retorna si el post está guardado

--- notiUnach/backend/routes/isSaved.js
+++ notiUnach/backend/routes/isSaved.js
@@ -22,7 +22,11 @@ router.get('/:userId/:postId',async(req,res)=>{
 
     try{
         
-
+        const [rows]=await db.query('SELECT 1 FROM saved_post WHERE user_id = ? AND post_id = ?',[userId,postId]);
+        const isSaved=rows.length > 0;
+        return res.status(200).json(jsonResponse(200,{
+            isSaved
+        }));
     }
     catch(error){
         console.error("Ha ocurrido un error en la consulta");
