a5ea3e837c3987af8e2b5afff908fb36713035b0
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-11-02 10:51:30 -0600

    Ya puedo retornar los comentarios de cada post, hace falta implementarlo

--- notiUnach/backend/routes/getComments.js
+++ notiUnach/backend/routes/getComments.js
@@ -12,9 +12,48 @@ const db = mysql.createPool({
 });
 
 router.get('/:postId',async(req,res)=>{
-    return res.status(200).json(jsonResponse(200,{
-        message:"Todo funcionando aquí"
-    }));
+    const { postId }=req.params;
+
+    if(!postId){
+        return res.status(400).json(jsonResponse(400,{
+            message:"Hace falta un dato"
+        }));
+    }
+
+    try{
+        const [rows]=await db.query(`
+            SELECT
+            comments.content,
+            comments.date,
+            user.name,
+            user.lastName,
+            multimedia.archive_path AS profile_picture
+            FROM comments
+            JOIN user ON comments.user_id = user.id
+            LEFT JOIN user_profile_picture ON user.id = user_profile_picture.user_id
+            AND user_profile_picture.is_using = TRUE
+            LEFT JOIN multimedia ON user_profile_picture.multimedia_id = multimedia.id
+            WHERE comments.post_id = ?
+            ORDER BY comments.date DESC, comments.id    
+        `,[postId]);
+
+        if(rows.length===0){
+            return res.status(404).json(jsonResponse(404,{
+                message:"Este post no contiene comentarios"
+            }));
+        }
+
+        return res.status(200).json(jsonResponse(200,{
+            comments:rows
+        }));
+    }
+    catch(error){
+        console.error('Ha ocurrido un error en la consulta: ',error);
+        return res.status(500).json(jsonResponse(500,{
+            error:"Ocurrió un error en la comunicación"
+        }));
+    }
+
 });
 
 module.exports=router;
