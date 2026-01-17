a2e43b13b4e2d3bfa783b8966bd11bf915bb1a3a
Alejandro Cruz <137844395+Alex-CruzC96@users.noreply.github.com>
2026-01-17 17:58:43 -0600

    Cambié la consulta para adecuarla a postgres

--- notiUnach/backend/routes/getAllPosts.js
+++ notiUnach/backend/routes/getAllPosts.js
@@ -8,21 +8,23 @@ router.get('/',async (req,res)=>{
 
     try{
 
-        const [rows]=await db.query(
-            `SELECT post.*, user.name, user.lastName, multimedia.archive_path AS profile_picture
-            FROM post
-            JOIN user ON post.user_id = user.id
-            LEFT JOIN user_profile_picture ON user.id = user_profile_picture.user_id 
-            AND user_profile_picture.is_using = TRUE
-            LEFT JOIN multimedia ON user_profile_picture.multimedia_id = multimedia.id
-            ORDER BY post.date DESC, post.id DESC
+        const result = await db.query(
+            `SELECT p.*, u.name, u.last_name, m.archive_path AS profile_picture
+            FROM posts p
+            JOIN users u ON p.user_id = u.id
+            LEFT JOIN user_profile_picture upp ON u.id = upp.user_id 
+            AND upp.is_using = TRUE
+            LEFT JOIN multimedia m ON upp.multimedia_id = m.id
+            ORDER BY p.created_at DESC, p.id DESC
             `
         );
 
+        const rows = result.rows;
+
         if(rows.length === 0){
-            return res.status(404).json(jsonResponse(404,{
+            return res.status(404).json({
                 message:"Aún no se ha realizado ninguna publicación"
-            }));
+            });
         }
 
         return res.status(200).json(jsonResponse(200,{
