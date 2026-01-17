8780e7d109f11bc95ccce317c174c9c15b6dbfd3
Alejandro Cruz <137844395+Alex-CruzC96@users.noreply.github.com>
2026-01-17 17:09:45 -0600

    Eliminé la variable DB del index por innecesaria

--- notiUnach/backend/index.js
+++ notiUnach/backend/index.js
@@ -1,7 +1,6 @@
 const express=require('express');
 const cors=require('cors');
 const app=express();
-const db=require('./lib/db');
 const path=require('path');
 
 const port=process.env.PORT || 3000;
